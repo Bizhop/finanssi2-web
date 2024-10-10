5d77a0d7762379ffe6cfb539b1d05f7b0250ac3a
Ville Piispa <ville.piispa@gmail.com>
2024-10-10 16:49:21 +0300

    added chat message sending and handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useSubscription } from "react-stomp-hooks"
 import { z } from "zod"
 
@@ -8,7 +8,11 @@ const ChatMessageSchema = z.object({
     timestamp: z.number()
 })
 
+const ChatMessageSchemaArray = z.array(ChatMessageSchema)
+
 type ChatMessage = z.infer<typeof ChatMessageSchema>
+
+const idToken = process.env.ID_TOKEN
 
 const App = () =>
     <div>
@@ -19,19 +23,60 @@ const App = () =>
 const Chat = () => {
     const [messages, setMessages] = useState<ChatMessage[]>([])
 
-    const addMessage = (jsonString: string) => {
+    useEffect(() => {
+        fetch('http://localhost:8080/api/chat', {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${idToken}`
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                const safeParsed = ChatMessageSchemaArray.parse(response)
+                setMessages(safeParsed)
+            })
+            .catch(console.error)
+    }, [])
+
+    const sendChatMessage = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const form = event.target as HTMLFormElement
+        const message = (form.querySelector('input[name="message"]') as HTMLInputElement).value
+        const messageData = {message}
+
+        fetch('http://localhost:8080/api/chat', {
+            method: 'POST',
+            body: JSON.stringify(messageData),
+            headers: {
+                "Authorization": `Bearer ${idToken}`,
+                "Content-Type": "application/json"
+            }
+        })
+
+        form.reset()
+    }
+
+    const receiveMessage = (jsonString: string) => {
         const parsedObject = JSON.parse(jsonString)
         const safeParsed = ChatMessageSchema.safeParse(parsedObject)
 
-        if(safeParsed.success) {
-            setMessages(prevMessages => [...prevMessages, safeParsed.data])
+        if (safeParsed.success) {
+            setMessages(prevMessages => {
+                const addedToMessageArray = [...prevMessages, safeParsed.data]
+                return addedToMessageArray.length > 20 ? addedToMessageArray.slice(-20) : addedToMessageArray
+            })
         }
     }
 
-    useSubscription("/topic/chat", message => addMessage(message.body))
+    useSubscription("/topic/chat", message => receiveMessage(message.body))
 
     return <div>
-        {messages.map(msg => <p>{msg.username} at {msg.timestamp}: {msg.message}</p>)}
+        <form onSubmit={sendChatMessage}>
+            <input type="text" name="message"></input>
+            <button type="submit">Send</button>
+        </form>
+        {messages.map((msg, index) => <p key={`chat-message-${index}`}>{index + 1} - <strong>{msg.username}:</strong> {msg.message}</p>)}
     </div>
 }
 
